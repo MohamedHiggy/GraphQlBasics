a4df6a98e89a9b73f23158f94142998a8c1b7dd1
hagooog <mohamed.higgy31@gmail.com>
2021-05-31 20:20:46 +0200

    add apollo and config file

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'https://api.spacex.land/graphql'
+})
 
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+})
+Vue.use(VueApollo)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    apolloProvider,
+    render: h => h(App)
 }).$mount('#app')
